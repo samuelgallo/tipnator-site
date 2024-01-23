9a35484c1baae7a7f5f6a0d97574001313214586
Samuel Gallo <samuel@samuelgallo.com>
2024-01-22 20:58:17 -0500

    SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -7,12 +8,46 @@ export const metadata = {
   title: "Tipnator: Easy Tip Calculation",
   description:
     "Tipnator: Your chill sidekick for stress-free tip calculations. Easy, breezy, and as relaxed as it gets – because tipping should be as simple as a summer day. Cheers to the easiest way to calculate your tips!",
+  alternates: {
+    canonical: "https://tipnator.com/",
+  },
+  openGraph: {
+    title: "Tipnator: Easy Tip Calculation",
+    description:
+      "Tipnator: Your chill sidekick for stress-free tip calculations. Easy, breezy, and as relaxed as it gets – because tipping should be as simple as a summer day. Cheers to the easiest way to calculate your tips!",
+    url: "https://tipnator.com/",
+    siteName: "Next.js",
+    images: [
+      {
+        url: "https://www.tipnator.com/icon.png",
+        width: 1024,
+        height: 1024,
+        alt: "Tipnator",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JC8Z0QDCN6"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-JC8Z0QDCN6');
+        `}
+        </Script>
+      </body>
     </html>
   );
 }
